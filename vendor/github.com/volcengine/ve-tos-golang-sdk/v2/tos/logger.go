ad5f040c23c0e0493dba38b1ebb4225fd6715db2
Olli Janatuinen <olli.janatuinen@gmail.com>
2025-05-29 16:43:57 +0000

    Remove vendoring and non-mandatory files

--- vendor/github.com/volcengine/ve-tos-golang-sdk/v2/tos/logger.go
+++
@@ -1,9 +0,0 @@
-package tos
-
-type Logger interface {
-	Debug(args ...interface{})
-	Info(args ...interface{})
-	Warn(args ...interface{})
-	Error(args ...interface{})
-	Fatal(args ...interface{})
-}
